d7c30011154e32369efc523e776e347ec3142429
walerij <becwal@yandex.ru>
2019-04-22 22:52:44 +0300

    Создание класса

--- index.php
+++ index.php
@@ -1,31 +1,10 @@
 <?php
   // Параметр $number - сообщает число 
   // символов в пароле
-  echo generate_password(9);
 
-  function generate_password($number)
-  {
-    $arr = array('a','b','c','d','e','f',
-                 'g','h','i','j','k','l',
-                 'm','n','o','p','r','s',
-                 't','u','v','x','y','z',
-                 'A','B','C','D','E','F',
-                 'G','H','I','J','K','L',
-                 'M','N','O','P','R','S',
-                 'T','U','V','X','Y','Z',
-                 '1','2','3','4','5','6',
-                 '7','8','9','0','.',',',
-                 '(',')','[',']','!','?',
-                 '&','^','%','@','*','$',
-                 '<','>','/','|','+','-',
-                 '{','}','`','~');
-    // Генерируем пароль
-    $pass = "";
-    for($i = 0; $i < $number; $i++)
-    {
-      // Вычисляем случайный индекс массива
-      $index = rand(0, count($arr) - 1);
-      $pass .= $arr[$index];
-    }
-    return $pass;
-  }
+namespase info;
+$pass= new passgen();
+    
+echo $pass->generate_password(9);
+
+  
